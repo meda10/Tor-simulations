6d5e80f8f5514a7dd8ff33d413d646f79afd5473
meda10 <meda735@gmail.com>
2019-03-09 14:09:12 +0100

    fix generator + graph

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ try:
     import stem.descriptor
     import stem.util.str_tools
     import ntor
+    from graphviz import Digraph
+    from collections import namedtuple
     from stem.descriptor.server_descriptor import RelayDescriptor, _truncated_b64encode
     from stem.descriptor.extrainfo_descriptor import RelayExtraInfoDescriptor
     from stem.descriptor.networkstatus import NetworkStatusDocumentV3
@@ -57,7 +59,8 @@ def write_descriptor(desc, filename):
 
 
 def parse_out():
-    output_file_path = '//home//petr//TorPs//output'
+    # output_file_path = '//home//petr//TorPs//output'
+    output_file_path = '//home//petr//PycharmProjects//Descriptors//generator_out//output'
     with open(output_file_path, 'r+') as fh:
         lines = fh.readlines()
         fh.close()
@@ -65,6 +68,7 @@ def parse_out():
     guard_node = ['Guard']
     middle_node = ['Middle']
     exit_node = ['Exit']
+    path = []
     
     for i in range(0, len(lines)):
         if lines[i].split()[2] not in guard_node and not lines[i].split()[2].__eq__('Guard'):
@@ -73,10 +77,14 @@ def parse_out():
             middle_node.append(lines[i].split()[3])
         if lines[i].split()[4] not in exit_node and not lines[i].split()[4].__eq__('Middle'):
             exit_node.append(lines[i].split()[4])
+        if not lines[i].split()[2].__eq__('Guard'):
+            path.append((lines[i].split()[2], lines[i].split()[3], lines[i].split()[4]))
     
     print(guard_node)
     print(middle_node)
     print(exit_node)
+    
+    return path
 
 
 def generate_router_status_entry(self, flags='Fast Running Stable Valid'):
@@ -225,13 +233,140 @@ def make_descriptors(guard_node=0, middle_node=0, exit_node=0):
     write_descriptor(consensus, 'consensus')
     write_descriptors(server_descriptors, 'server-descriptors')
 
+    return consensus_entries
+
+
+def generate_graph(routers, paths):
+    MyStruct = namedtuple("path", "guard middle exit")
+    x = MyStruct(guard=('Unnamed1548', '214.215.85.37'),
+                 middle=('Unnamed1549', '192.255.55.25'),
+                 exit=('Unnamed1550', '87.84.85.37'))
+    
+    guard_node = []
+    middle_node = []
+    exit_node = []
+    
+    dot = Digraph(comment='Nodes')
+    
+    for r in routers:
+        if "Guard" in r.flags:
+            guard_node.append(r.address)
+        elif "Exit" in r.flags:
+            exit_node.append(r.address)
+        else:
+            middle_node.append(r.address)
+        dot.node(r.address, r.address)
+    
+    for g in guard_node:
+        print(g)
+    
+    g = ('; '.join(str(g) for g in guard_node))
+    e = ('; '.join(str(e) for e in exit_node))
+    m = ('; '.join(str(m) for m in middle_node))
+    
+    dot.attr(rank="same; " + g)
+    dot.attr(rank="same; " + e)
+    dot.attr(rank="same; " + m)
+    
+    for path in paths:
+        dot.edge(path[0], path[1])
+        dot.edge(path[1], path[2])
+    
+    dot.render('test-output/simple_simulation.gv', view=False)
+
+
+def graph_test_generator(routers, paths):
+    guard_node = []
+    middle_node = []
+    exit_node = []
+    
+    # graph = Digraph(comment='Nodes')
+    graph = Digraph('test', format='png')
+    
+    subgraph_guards = Digraph('subgraph_guards')
+    subgraph_middles = Digraph('subgraph_middles')
+    subgraph_exits = Digraph('subgraph_exits')
+    
+    subgraph_guards.graph_attr.update(rank='same')
+    subgraph_middles.graph_attr.update(rank='same')
+    subgraph_exits.graph_attr.update(rank='same')
+    
+    for r in routers:
+        if "Guard" in r.flags:
+            # guard_node.append(r.address)
+            subgraph_guards.node(str(r.address), color='red')
+        elif "Exit" in r.flags:
+            # exit_node.append(r.address)
+            subgraph_exits.node(str(r.address), color='green')
+        else:
+            # middle_node.append(r.address)
+            subgraph_middles.node(str(r.address), color='blue')
+    
+    for path in paths:
+        graph.edge(path[0], path[1])
+        graph.edge(path[1], path[2])
+    
+    graph.subgraph(subgraph_guards)
+    graph.subgraph(subgraph_middles)
+    graph.subgraph(subgraph_exits)
+    
+    graph.render('test-output/simple_simulation.gv', view=True)
+
+
+def run_simulation():
+    dir_path = '//home//petr//TorPs//out//network-state-2019-02'
+    output_file_path = '//home//petr//PycharmProjects//Descriptors//generator_out//output'
+    num_samples = '5'
+    tracefile = '//home//petr//TorPs//in//users2-processed.traces.pickle'
+    usermodel = 'simple=6000'
+    format_arg = 'normal'
+    adv_guard_bw = '0'
+    adv_exit_bw = '0'
+    adv_time = '0'
+    num_adv_guards = '0'
+    num_adv_exits = '0'
+    num_guards = '1'
+    gard_expiration = '80000000'
+    loglevel = 'INFO'
+    path_alg = 'tor'
+    
+    start_year = '2019'
+    end_year = '2019'
+    start_month = '2'
+    end_month = '2'
+    in_dir = '//home//petr//TorPs//in'
+    out_dir = '//home//petr//TorPs//out'
+    initial_descriptors_dir = '//home//petr//TorPs//in//server-descriptors-2019-02'
+    
+    # os.system("python //home//petr//TorPs//pathsim.py simulate --help")
+    
+    os.system("python //home//petr//TorPs//pathsim.py process --start_year {} --start_month {} --end_year {} "
+              "--end_month {} --in_dir {} --out_dir {} --initial_descriptor_dir {} > /dev/null 2>&1"
+              .format(start_year, start_month, end_year, end_month, in_dir, out_dir, initial_descriptors_dir))
+    
+    os.system("python //home//petr//TorPs//pathsim.py simulate --nsf_dir {} --num_samples {} --trace_file {} "
+              "--user_model {} --format {} --adv_guard_cons_bw {} --adv_exit_cons_bw {} --adv_time {} "
+              "--num_adv_guards {} --num_adv_exits {} --num_guards {} --guard_expiration {} --loglevel {} {} > {}"
+              .format(dir_path, num_samples, tracefile, usermodel, format_arg, adv_guard_bw, adv_exit_bw, adv_time,
+                      num_adv_guards, num_adv_exits, num_guards, gard_expiration, loglevel, path_alg, output_file_path))
+    
+    """
+    os.system("python //home//petr//TorPs//pathsim.py simulate "
+              "--nsf_dir //home//petr//TorPs//out//network-state-2019-02 --num_samples 1 "
+              "--user_model simple=600 --format normal tor")
+    """
+
 
 if __name__ == '__main__':
     # if len(sys.argv) < 2 or not sys.argv[1].isdigit():
     #     print('We need a numeric argument indicating how many descriptors to make.')
     #    sys.exit(1)
 
-    make_descriptors(5, 5, 5)
-    parse_out()
+    routers = make_descriptors(1, 5, 1)
+    run_simulation()
+    paths = parse_out()
+    graph_test_generator(routers, paths)
+    # generate_graph(routers, paths)
+    
     # create_server_descriptors()
     # create_consensus()
